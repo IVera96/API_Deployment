a587c57726f8b83e613dc0ef8669cbb0631af1e5
IVera96 <ilas.vera96@gmail.com>
2024-12-18 11:12:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -61,43 +61,8 @@ required_features = [
     "Prix_m2_prov",
 ]
 
-# Verifica se i dati dell'utente sono completi e validi prima di proseguire
 if not validate_inputs(user_selections, required_features):
     st.stop()
-
-
-# if st.button("Calculate Price 🏚️"):
-#     if validate_inputs(user_selections, required_features):
-#         # Procedi con il calcolo
-
-# # Create DataFrame from user input
-#         user_df = pd.DataFrame([user_selections])
-#         # st.write("Dataframe prima della   trasformazione:", user_df)
-#         # Apply necessary transformations (Label Encoding)
-#         user_df=preprocess_user_input(user_df)
-#         # st.write("Dataframe dopo la  trasformazione:", user_df)
-#         # Add a new features because my scaler expects 14 features and the user_df have have only 13
-#         user_df.insert(0, 'target', 0)
-
-
-#         X_user=np.array(user_df)
-#         X_user_scaled = scaler.transform(X_user)
-
-#         #Select only the 13 features
-#         X_user_selected = X_user_scaled[0][1:]
-
-#         X_test_reshaped =X_user_selected.reshape(1, -1)
-
-#         result=model.predict(X_test_reshaped)
-#         # print(result)
-
-#         prediction = my_inverse_scaler(result, X_user_selected)
-#         pred_result = round(prediction[0], 2)
-#         print("Predizione finale:", round(prediction[0],2))
-#         st.metric(label=f"Price 🏚️:", value=f"{pred_result}", delta="Euros (€)")
-
-#     else:
-#         st.error("Some required inputs are missing. Please check your selections.")
 
 
 def main():
@@ -120,8 +85,8 @@ def main():
         )
         col1, col2, col3 = st.columns(
             [1, 2, 1]
-        )  # Creazione di colonne (centrale più larga)
-        with col2:  # Mettere il widget metric nella colonna centrale
+        ) 
+        with col2: 
             if st.button("Calculate Price 🏚️"):
                 if validate_inputs(user_selections, required_features):
                     # Procedi con il calcolo
@@ -147,7 +112,7 @@ def main():
 
                     prediction = my_inverse_scaler(result, X_user_selected)
                     pred_result = round(prediction[0], 2)
-                    print("Predizione finale:", round(prediction[0], 2))
+                    print("Predition final:", round(prediction[0], 2))
                     st.metric(
                         label=f"Price 🏚️:", value=f"{pred_result}", delta="Euros (€)"
                     )
@@ -161,19 +126,17 @@ def main():
         st.markdown("<hr/>", unsafe_allow_html=True)
         st.markdown("### Graphs Area 📊")
 
-        # Prima riga con il primo grafico
         col1, col2, col3 = st.columns([1, 2, 1])  # Colonna centrale più larga
         with col2:
             st.markdown("#### Price Distribution")
             create_histogram(df)
 
-        # Seconda riga con il secondo grafico
         col1, col2, col3 = st.columns([1, 2, 1])
         with col2:
             st.markdown("#### Price Distribution by Property Type")
             create_boxplot(df)
 
-        # Terza riga con il terzo grafico
+    
         col1, col2, col3 = st.columns([1, 2, 1])
         with col2:
             st.markdown("#### Prices by Municipality")
