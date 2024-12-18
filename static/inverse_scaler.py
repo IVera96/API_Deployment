b1d26d73b7dee97e3e20affc52a61684140302d8
IVera96 <ilas.vera96@gmail.com>
2024-12-18 10:30:29 +0100

    Update inverse_scaler.py

--- static/inverse_scaler.py
+++ static/inverse_scaler.py
@@ -10,8 +10,8 @@ def my_inverse_scaler(y: np.ndarray, X: np.ndarray) -> np.ndarray:
     """
     Inverse scale the prediction result.
     """
-    tmp = y.reshape((-1, 1))  # Reshape to 2D
-    X_reshaped = X.reshape(1, -1)  # Reshape X to 2D
+    tmp = y.reshape((-1, 1))  
+    X_reshaped = X.reshape(1, -1) 
     tmp = np.concatenate((tmp, X_reshaped), axis=1)
-    tmp = scaler.inverse_transform(tmp)  # Inverse scale
-    return tmp[:, 0]  # Return only the target column
+    tmp = scaler.inverse_transform(tmp) 
+    return tmp[:, 0] 
